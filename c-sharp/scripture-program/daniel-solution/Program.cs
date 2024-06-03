1f62751a68a819008bc2e5df344e83ff3f2b6e34
danielgomez3 <danielgomez3@verizon.net>
2024-06-03 11:07:49 -0400

    added gitignore

---
+++ c-sharp/scripture-program/daniel-solution/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+	    Console.WriteLine("Hello, World!");
+	    Console.WriteLine("Arguments passed to the program:");
+	    foreach (var arg in args)
+	    {
+	        Console.WriteLine(arg);
+	    }
+	}
+}
+	
