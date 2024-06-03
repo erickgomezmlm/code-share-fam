f1d0ddd8adeba524f6e98509295072badb42348b
danielgomez3 <danielgomez3@verizon.net>
2024-06-02 22:03:02 -0400

    made dumb directory structure changes

--- c-sharp/scripture-program/obj/Debug/net8.0/proj.AssemblyInfo.cs
+++ c-sharp/scripture-program/obj/Debug/net8.0/proj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c59abb5b3b4826ccc2632d63581432305e28a37")]
 [assembly: System.Reflection.AssemblyProductAttribute("proj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
